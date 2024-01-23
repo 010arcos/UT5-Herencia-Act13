932f59a25b961f34d0a5bfb037c7901c0b01929e
Marco <marco1arcos@gmail.com>
2024-01-23 22:55:16 +0100

    second commit

--- src/Act13/Main.java
+++ src/Act13/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         boolean salir= false;
 
+
         do {
             Scanner sc= new Scanner(System.in);
             System.out.println("-------------------------");
